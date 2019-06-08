cec6b9edeb55baf1da2a2b7da9379ad3d9769467
Lucas Sodré <lucas.teixeira@calindra.com.br>
2019-06-08 17:49:27 -0300

    Adicionados os esqueletos das páginas MONITOR e HISTÓRICO

--- src/app/pages/historico/historico.page.ts
+++ src/app/pages/historico/historico.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-historico',
@@ -7,9 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HistoricoPage implements OnInit {
 
-  constructor() { }
+  public monitorHistory: any;
+
+  constructor(public navCtrl: NavController) {
+    this.monitorHistory = [
+      {
+        corrente: 'Corrente: ', valorCorrente: 2,
+        tensao: 'Tensão:', valorTensao: '220v',
+        tempoAR: 'Tempo de A.A.:', valorAA: '4h32m21s',
+      },
+      {
+        corrente: 'Corrente: ', valorCorrente: 5,
+        tensao: 'Tensão:', valorTensao: '127v',
+        tempoAR: 'Tempo de A.A.:', valorAA: '2h32m21s',
+      },
+      {
+        corrente: 'Corrente: ', valorCorrente: 10,
+        tensao: 'Tensão:', valorTensao: '220v',
+        tempoAR: 'Tempo de A.A.:', valorAA: '1h32m21s',
+      },
+      {
+        corrente: 'Corrente: ', valorCorrente: 12,
+        tensao: 'Tensão:', valorTensao: '127v',
+        tempoAR: 'Tempo de A.A.:', valorAA: '5h32m21s',
+      },
+      {
+        corrente: 'Corrente: ', valorCorrente: 10,
+        tensao: 'Tensão:', valorTensao: '220v',
+        tempoAR: 'Tempo de A.A.:', valorAA: '1h32m21s',
+      },
+      {
+        corrente: 'Corrente: ', valorCorrente: 12,
+        tensao: 'Tensão:', valorTensao: '127v',
+        tempoAR: 'Tempo de A.A.:', valorAA: '5h32m21s',
+      }
+    ]
+  }
 
   ngOnInit() {
   }
-
 }
--- src/app/pages/monitor/monitor.page.ts
+++ src/app/pages/monitor/monitor.page.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-monitor',
   templateUrl: './monitor.page.html',
   styleUrls: ['./monitor.page.scss'],
 })
-export class MonitorPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class MonitorPage {
+  public items: any;
+  public machines: any;
+  constructor(public navCtrl: NavController) {
+    this.items = [
+      { title: 'Corrente: ', valor: 2 },
+      { title: 'Tensão:', valor: '127v' },
+      { title: 'Tempo de A.A.: ', valor: '2h32m21s' },
+    ],
+    this.machines = [
+      {title: 'Máquina de soldagem Arc2350'}
+    ]
   }
-
 }
