49b831d6ac2ce959353642b8c6d6e1f423430554
Lucas Sodré <lucas.teixeira@calindra.com.br>
2019-06-08 23:18:46 -0300

    Criado front v1

--- src/app/pages/historico/historico.page.ts
+++ src/app/pages/historico/historico.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-historico',
@@ -6,8 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./historico.page.scss'],
 })
 export class HistoricoPage implements OnInit {
+  public monitorHistory: any;
 
-  constructor() { }
+  constructor(public navCtrl: NavController) {
+    this.monitorHistory = [
+      {
+        corrente: 'Corrente: ', valorCorrente: 2,
+        tensao: 'Tensão:', valorTensao: '220v',
+        tempoAR: 'Tempo de A.A.:', valorAA: '4h32m21s',
+      },
+      {
+        corrente: 'Corrente: ', valorCorrente: 5,
+        tensao: 'Tensão:', valorTensao: '127v',
+        tempoAR: 'Tempo de A.A.:', valorAA: '2h32m21s',
+      },
+      {
+        corrente: 'Corrente: ', valorCorrente: 10,
+        tensao: 'Tensão:', valorTensao: '220v',
+        tempoAR: 'Tempo de A.A.:', valorAA: '1h32m21s',
+      },
+      {
+        corrente: 'Corrente: ', valorCorrente: 12,
+        tensao: 'Tensão:', valorTensao: '127v',
+        tempoAR: 'Tempo de A.A.:', valorAA: '5h32m21s',
+      },
+      {
+        corrente: 'Corrente: ', valorCorrente: 10,
+        tensao: 'Tensão:', valorTensao: '220v',
+        tempoAR: 'Tempo de A.A.:', valorAA: '1h32m21s',
+      },
+      {
+        corrente: 'Corrente: ', valorCorrente: 12,
+        tensao: 'Tensão:', valorTensao: '127v',
+        tempoAR: 'Tempo de A.A.:', valorAA: '5h32m21s',
+      }
+    ]
+  }
 
   ngOnInit() {
   }
--- src/app/pages/sobre/sobre.page.ts
+++ src/app/pages/sobre/sobre.page.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-sobre',
   templateUrl: './sobre.page.html',
   styleUrls: ['./sobre.page.scss'],
 })
-export class SobrePage implements OnInit {
+export class SobrePage {
 
-  constructor() { }
-
-  ngOnInit() {
+  public team: any;
+  constructor(public navCtrl: NavController) {
+    this.team = [
+      { name: 'Lucas', age: 23, period: '7º período' },
+      { name: 'Ricardo', age: 99, period: '8º período' },
+    ]
   }
 
 }
