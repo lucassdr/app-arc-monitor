8f271938d860d6d9c9fd2e91b70e984ecbe7337e
Lucas Sodré <lucas.teixeira@calindra.com.br>
2019-06-08 22:05:30 -0300

    Revert "Adicionados os esqueletos das páginas MONITOR e HISTÓRICO"
    This reverts commit cec6b9edeb55baf1da2a2b7da9379ad3d9769467.

--- src/app/pages/historico/historico.page.ts
+++ src/app/pages/historico/historico.page.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { MenuController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-historico',
@@ -8,43 +7,9 @@ import { MenuController, NavController } from '@ionic/angular';
 })
 export class HistoricoPage implements OnInit {
 
-  public monitorHistory: any;
-
-  constructor(public navCtrl: NavController) {
-    this.monitorHistory = [
-      {
-        corrente: 'Corrente: ', valorCorrente: 2,
-        tensao: 'Tensão:', valorTensao: '220v',
-        tempoAR: 'Tempo de A.A.:', valorAA: '4h32m21s',
-      },
-      {
-        corrente: 'Corrente: ', valorCorrente: 5,
-        tensao: 'Tensão:', valorTensao: '127v',
-        tempoAR: 'Tempo de A.A.:', valorAA: '2h32m21s',
-      },
-      {
-        corrente: 'Corrente: ', valorCorrente: 10,
-        tensao: 'Tensão:', valorTensao: '220v',
-        tempoAR: 'Tempo de A.A.:', valorAA: '1h32m21s',
-      },
-      {
-        corrente: 'Corrente: ', valorCorrente: 12,
-        tensao: 'Tensão:', valorTensao: '127v',
-        tempoAR: 'Tempo de A.A.:', valorAA: '5h32m21s',
-      },
-      {
-        corrente: 'Corrente: ', valorCorrente: 10,
-        tensao: 'Tensão:', valorTensao: '220v',
-        tempoAR: 'Tempo de A.A.:', valorAA: '1h32m21s',
-      },
-      {
-        corrente: 'Corrente: ', valorCorrente: 12,
-        tensao: 'Tensão:', valorTensao: '127v',
-        tempoAR: 'Tempo de A.A.:', valorAA: '5h32m21s',
-      }
-    ]
-  }
+  constructor() { }
 
   ngOnInit() {
   }
+
 }
--- src/app/pages/monitor/monitor.page.ts
+++ src/app/pages/monitor/monitor.page.ts
@@ -1,22 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-monitor',
   templateUrl: './monitor.page.html',
   styleUrls: ['./monitor.page.scss'],
 })
-export class MonitorPage {
-  public items: any;
-  public machines: any;
-  constructor(public navCtrl: NavController) {
-    this.items = [
-      { title: 'Corrente: ', valor: 2 },
-      { title: 'Tensão:', valor: '127v' },
-      { title: 'Tempo de A.A.: ', valor: '2h32m21s' },
-    ],
-    this.machines = [
-      {title: 'Máquina de soldagem Arc2350'}
-    ]
+export class MonitorPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
   }
+
 }
